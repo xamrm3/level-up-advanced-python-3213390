7bb5430d4711afac4e914355f938f86ee63b155e
xamrm3 <110181628+xamrm3@users.noreply.github.com>
2025-08-26 19:30:23 +0000

    completed html2markdown

--- challenge/html2markdown.py
+++ challenge/html2markdown.py
@@ -1,4 +1,21 @@
 
 def html2markdown(html):
     '''Take in html text as input and return markdown'''
-    pass
+
+    replaced = html.replace("<em>", "*").replace("</em>", "*")
+    
+    replaced = replaced.replace("\n", " ").replace("</p><p>", "\n\n").replace("<p>", "").replace("</p>", "")
+
+    while "  " in replaced:
+        replaced = replaced.replace("  ", " ")
+
+    while replaced.find("</a>") != -1:
+        start_link = replaced.find('<a href="')
+        end_link = replaced.find('">', start_link)
+        url = replaced[start_link + 9:end_link]
+        end_tag = replaced.find("</a>", end_link)
+        link_text = replaced[end_link + 2:end_tag]
+        markdown_link = f"[{link_text}]({url})"
+        replaced = replaced[:start_link] + markdown_link + replaced[end_tag + 4:]
+
+    return replaced
