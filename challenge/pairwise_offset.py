90331c8d9c19c0ddcc058c795ae05fac27a604ca
xamrm3 <110181628+xamrm3@users.noreply.github.com>
2025-08-27 18:58:13 +0000

    complete pairwise_offset

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,4 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offset(sequence, fillvalue="*", offset=0):
+    cpy1 = list(sequence) + [fillvalue] * offset
+    cpy2 = [fillvalue] * offset + list(sequence)
+    return zip(cpy1, cpy2)
