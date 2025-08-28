5eb22703ae9a0e0f6ed02b3308f6b1d8f813f23d
xamrm3 <110181628+xamrm3@users.noreply.github.com>
2025-08-28 19:37:04 +0000

    completed calculator

--- challenge/calculator.py
+++ challenge/calculator.py
@@ -1,2 +1,13 @@
 class Calculator:
-    pass
+    def __init__(self, type_error, value_error, name_error, zero_division_error):
+        self.type_error = type_error
+        self.value_error = value_error
+        self.name_error = name_error
+        self.zero_division_error = zero_division_error
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.error = exc_value
+        return exc_value
