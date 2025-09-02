fa8ca364ef6457ffc06d93cef7cf6f631201cad9
xamrm3 <110181628+xamrm3@users.noreply.github.com>
2025-09-02 20:25:21 +0000

    completed medals

--- challenge/medals.py
+++ challenge/medals.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
+import re
 
-with open('olympics.txt', 'rt', encoding='utf-8') as file:    
+with open('challenge/olympics.txt', 'rt', encoding='utf-8') as file:    
     olympics = file.read()
 
 medal = namedtuple('medal', ['City', 'Edition', 'Sport', 'Discipline', 'Athlete', 'NOC', 'Gender',
@@ -8,6 +9,14 @@ medal = namedtuple('medal', ['City', 'Edition', 'Sport', 'Discipline', 'Athlete'
 
 medals = [] #Complete this - medals is a list of medal namedtuples
 
+for line in re.findall(r'.+;.+', olympics)[1:]:
+    medals.append(medal(*line.split(";")))
+
 def get_medals(**kwargs):
     '''Return a list of medal namedtuples '''
-    pass
+    result = []
+    for medal in medals:
+        if all(getattr(medal, k) == v for k,v in kwargs.items()):
+            result.append(medal)
+    return result
+
